a0ed84c54d57e43b0bbe940356de952ae1fe7ee9
yeon17s <yeons7j@gmail.com>
2026-04-30 17:58:40 +0900

    fix: ROS connection lifecycle 안정화
    - useRosConnection: batterySub/poseSub를 외부 스코프로 이동, unsubscribeAll() 추가
    - reconnect 및 cleanup 시 subscriber 명시적 unsubscribe로 누수 방지

--- src/hooks/useRosConnection.ts
+++ src/hooks/useRosConnection.ts
@@ -1,48 +1,84 @@
 import * as ROSLIB from "roslib";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import useRobotStore from "../store/robotStore";
 import useSettingsStore from "../store/settingsStore";
+import { setRos } from "../lib/rosClient";
+
+const RECONNECT_DELAY_MS = 3000;
 
 export default function useRosConnection() {
-  const rosRef = useRef<ROSLIB.Ros | null>(null);
+  const jetsonIp      = useSettingsStore((s) => s.jetsonIp);
+  const rosbridgePort = useSettingsStore((s) => s.rosbridgePort);
 
   useEffect(() => {
-    const { jetsonIp, rosbridgePort } = useSettingsStore.getState();
     const { setConnectionStatus, setBatteryPercent, setPose } = useRobotStore.getState();
 
-    const ros = new ROSLIB.Ros({ url: `ws://${jetsonIp}:${rosbridgePort}` });
-    rosRef.current = ros;
+    let ros: ROSLIB.Ros;
+    let reconnectTimer: ReturnType<typeof setTimeout> | null = null;
+    let destroyed = false;
 
-    ros.on("connection", () => setConnectionStatus("rosConnected", true));
-    ros.on("error",      () => setConnectionStatus("rosConnected", false));
-    ros.on("close",      () => setConnectionStatus("rosConnected", false));
+    // 외부 스코프에서 subscriber를 관리해 reconnect 시 unsubscribe 가능하게 함
+    let batterySub: ROSLIB.Topic | null = null;
+    let poseSub: ROSLIB.Topic | null = null;
 
-    const batterySub = new ROSLIB.Topic({
-      ros,
-      name: "/battery_state",
-      messageType: "sensor_msgs/BatteryState",
-    });
-    batterySub.subscribe((msg: any) => {
-      setBatteryPercent(Math.round(msg.percentage * 100));
-    });
+    function unsubscribeAll() {
+      batterySub?.unsubscribe();
+      poseSub?.unsubscribe();
+      batterySub = null;
+      poseSub = null;
+    }
 
-    const poseSub = new ROSLIB.Topic({
-      ros,
-      name: "/amcl_pose",
-      messageType: "geometry_msgs/PoseWithCovarianceStamped",
-    });
-    poseSub.subscribe((msg: any) => {
-      const { x, y } = msg.pose.pose.position;
-      const { z, w } = msg.pose.pose.orientation;
-      setPose({ x, y, yaw: 2 * Math.atan2(z, w) });
-    });
+    function connect() {
+      ros = new ROSLIB.Ros({ url: `ws://${jetsonIp}:${rosbridgePort}` });
+      setRos(ros);
+
+      ros.on("connection", () => {
+        setConnectionStatus("rosConnected", true);
+
+        batterySub = new ROSLIB.Topic({
+          ros,
+          name: "/battery_state",
+          messageType: "sensor_msgs/BatteryState",
+        });
+        batterySub.subscribe((msg: any) => {
+          setBatteryPercent(Math.round(msg.percentage * 100));
+        });
+
+        poseSub = new ROSLIB.Topic({
+          ros,
+          name: "/amcl_pose",
+          messageType: "geometry_msgs/PoseWithCovarianceStamped",
+        });
+        poseSub.subscribe((msg: any) => {
+          const { x, y } = msg.pose.pose.position;
+          const { z, w } = msg.pose.pose.orientation;
+          setPose({ x, y, yaw: 2 * Math.atan2(z, w) });
+        });
+      });
+
+      ros.on("error", () => {
+        setConnectionStatus("rosConnected", false);
+      });
+
+      ros.on("close", () => {
+        unsubscribeAll();
+        setConnectionStatus("rosConnected", false);
+        setRos(null);
+        if (!destroyed) {
+          reconnectTimer = setTimeout(connect, RECONNECT_DELAY_MS);
+        }
+      });
+    }
+
+    connect();
 
     return () => {
-      batterySub.unsubscribe();
-      poseSub.unsubscribe();
-      ros.close();
+      destroyed = true;
+      if (reconnectTimer) clearTimeout(reconnectTimer);
+      unsubscribeAll();
+      ros?.close();
+      setRos(null);
+      setConnectionStatus("rosConnected", false);
     };
-  }, []);
-
-  return rosRef;
+  }, [jetsonIp, rosbridgePort]);
 }
